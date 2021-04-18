d860524f3f92a76cfe13f4898ad7bfebf5829898
Joseph M <jmedina1985@hotmail.com>
2021-04-17 21:10:29 -0700

    display photos

--- src/Photo.js
+++ src/Photo.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
-const Photo = ({ urls: { regular }, alt_description }) => {
+const Photo = ({
+  urls: { regular },
+  alt_description,
+  likes,
+  user: {
+    name,
+    portfolio_url,
+    profile_url: { medium },
+  },
+}) => {
   return (
     <article classname="photo">
       <img src={regular} alt={alt_description} />
+      <div className="photo-info"></div>
     </article>
   );
 };
