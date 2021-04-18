aefdd2daee7d0562efee8e6fed0543de7180e7a3
Joseph M <jmedina1985@hotmail.com>
2021-04-17 21:11:01 -0700

    display photos

--- src/Photo.js
+++ src/Photo.js
@@ -7,13 +7,15 @@ const Photo = ({
   user: {
     name,
     portfolio_url,
-    profile_url: { medium },
+    profile_image: { medium },
   },
 }) => {
   return (
     <article classname="photo">
       <img src={regular} alt={alt_description} />
-      <div className="photo-info"></div>
+      <div className="photo-info">
+        <h4>{name}</h4>
+      </div>
     </article>
   );
 };
