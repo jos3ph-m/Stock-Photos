80084a9452bb51a040e34f738ce685cf581fdcc3
Joseph M <jmedina1985@hotmail.com>
2021-04-17 21:08:02 -0700

    display photos

--- src/Photo.js
+++ src/Photo.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from 'react';
 
-const Photo = () => {
-  return <h2>photo component</h2>
-}
+const Photo = ({}) => {
+  return <h2>photo component</h2>;
+};
 
-export default Photo
+export default Photo;
